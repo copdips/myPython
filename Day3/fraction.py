abe90d5807197391d0729d7be5a10f113c4f9078
copdips <xiang.zhu@outlook.com>
2017-06-01 17:17:49 +0200

    end of day3

--- Day3/fraction.py
+++ Day3/fraction.py
@@ -1,5 +1,5 @@
 class Fraction:
-
+    """ class Fraction """
     def __init__(self, N, D):
         self.num = N
         self.den = D
@@ -25,12 +25,21 @@ class Fraction:
     def __truediv__(self, autre):
         return Fraction(self.num * autre.den , self.den * autre.num)
 
+    def coucou(self):
+        """ coucou """
+        return "coucou " + str(self.num)
+
+    def coucou2(self):
+        """ coucou2 """
+        return "coucou2 " + str(self.num)
+
 f1 = Fraction(3, 4)
 
 print(type(f1))
 print(dir(f1))
 print(f1.num)
 print(f1)
+print(f1.coucou())
 
 f2 = Fraction(1, 4)
 
