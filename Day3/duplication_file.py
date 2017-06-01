045fc5f7e0165eb64872b06cf9416004f255dd19
copdips <xiang.zhu@outlook.com>
2017-06-01 15:25:24 +0200

    duplication_file.py ok

--- Day3/duplication_file.py
+++ Day3/duplication_file.py
@@ -2,32 +2,43 @@ def printFile(fileIn):
     with open(fileIn, mode="r", encoding='utf8') as myFileIn :
         print(myFileIn.read())
 
-def duplicate_file(src,dst,overwrite=False, append=False):
-    """ deplicate file"""
+def duplicate_file(overwrite=False, append=False,**args):
+    """
+        deplicate file
+        2 args are mandatory:
+        - src: the file to copy
+        - dst: the file to create
+    """
+    print("args:",args.keys())
+    for i in args.keys():
+        print(i,":",args[i])
+
+    if not {"src","dst"} <= args.keys():
+        raise TypeError("src and dst are the mandatory params")
 
     print("***duplicate file***")
 
-    with open(src, mode="r", encoding='utf8') as myOldFile:
+    with open(args["src"], mode="r", encoding='utf8') as myOldFile:
 
         if overwrite:
             open_mode = "w+"
         else:
             open_mode = "a"
 
-        with open(dst, mode=open_mode, encoding='utf8') as myNewFile:
+        with open(args["dst"], mode=open_mode, encoding='utf8') as myNewFile:
             myNewFile.writelines(myOldFile.readlines())
 
-    print("oldFile :", src)
-    printFile(src)
+    print("oldFile :", args["src"])
+    printFile(args["src"])
 
-    print("newFile :", dst)
-    printFile(dst)
+    print("newFile :", args["dst"])
+    printFile(args["dst"])
 
 
 
 try :
-    duplicate_file("1.txt","2.txt")
-    duplicate_file("1.txt","2.txt",overwrite=True)
+    duplicate_file(src="1.txt",dst="2.txt")
+    # duplicate_file("1.txt","2.txt",overwrite=True)
     # duplicate_file("1.mp3","2.mp3",overwrite=True)
 except TypeError as e:
     print("TypeError:", e)
