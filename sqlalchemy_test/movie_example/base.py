cb953e4ec9c26871e1b1c8a6efd378e5c89cffda
Xiang ZHU <xiang.zhu@outlook.com>
2019-03-24 18:54:56 +0100

    test sqlalchemy with sub and vm

--- sqlalchemy_test/movie_example/base.py
+++ sqlalchemy_test/movie_example/base.py
@@ -1,9 +1,37 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import Column, String, Integer, Date, Table, ForeignKey
 
 # engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
 engine = create_engine('sqlite:///moive_example.db')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
+
+# sub_vm_association = Table(
+#     "sub_vm", Base.metadata,
+#     Column("sub_vmname", String, ForeignKey("sub.vmname")),
+#     Column("vm_vmname", String, ForeignKey("vm.vmname"))
+# )
+
+
+class Sub(Base):
+    __tablename__ = 'sub'
+
+    subid = Column(String, primary_key=True)
+    subname = Column(String)
+    vmname = Column(String)
+    # vms = relationship("VM",  secondary=sub_vm_association)
+    eco = Column(String)
+
+
+class VM(Base):
+    __tablename__ = 'vm'
+
+    vmid = Column(String, primary_key=True)
+    # subid = Column(String, ForeignKey("sub.subid"))
+    vmname = Column(String)
+    # vmname = relationship("Sub", backref="vm")
+
