7a33e85ef1999a20f382f776d394cec0b78ff3db
Xiang ZHU <xiang.zhu@outlook.com>
2020-02-01 15:49:16 +0100

    add 31_quickstart_exe

---
+++ asyncio/31_quickstart_exe.py
@@ -0,0 +1,34 @@
+import time
+import asyncio
+
+
+async def main():
+    print(f"{time.ctime()} Hello!")
+    await asyncio.sleep(1.0)
+    print(f"{time.ctime()} Goodbye!")
+    loop.stop()
+
+
+def blocking():
+    # if this blocking sleep is larger thant 1 sec as defined in the main() asyncio.sleep, we will get an error.
+    time.sleep(0.5)
+    print(f"{time.ctime()} Hello from a thread!")
+
+
+loop = asyncio.get_event_loop()
+
+loop.create_task(main())
+loop.run_in_executor(None, blocking)
+
+loop.run_forever()
+
+pending = asyncio.Task.all_tasks(loop=loop)
+group = asyncio.gather(*pending)
+loop.run_until_complete(group)
+loop.close()
+
+# Output:
+# $ python quickstart_exe.py
+# Sun Sep 17 14:17:38 2017 Hello!
+# Sun Sep 17 14:17:38 2017 Hello from a thread!
+# Sun Sep 17 14:17:39 2017 Goodby
