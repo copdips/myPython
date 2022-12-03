881d284ccee452ce00e02462547c89966083bc55
Xiang ZHU <xiang.zhu@outlook.com>
2022-12-03 22:57:28 +0100

    add async_context.py

---
+++ asyncio/wupeiqi/async_context.py
@@ -0,0 +1,29 @@
+import asyncio
+
+
+class AsyncContextManager:
+    def __init__(self):
+        print("AsyncContextManager init")
+
+    async def do_something(self):
+        return 666
+
+    async def __aenter__(self):
+        print("AsyncContextManager __aenter__ in")
+        self.conn = await asyncio.sleep(1)
+        print("AsyncContextManager __aenter__ out")
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        print("AsyncContextManager __aexit__ in")
+        await asyncio.sleep(1)
+        print("AsyncContextManager __aexit__ out")
+
+
+async def func():
+    async with AsyncContextManager() as f:
+        result = await f.do_something()
+        print(result)
+
+
+asyncio.run(func())
