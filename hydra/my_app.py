b36b1207d5f6bdf11946deaa6b2fbb19b36aa625
Xiang ZHU <xiang.zhu@outlook.com>
2022-12-03 22:56:46 +0100

    add hydra

---
+++ hydra/my_app.py
@@ -0,0 +1,22 @@
+import os
+
+from omegaconf import DictConfig, OmegaConf
+
+import hydra
+
+
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def my_app(cfg: DictConfig):
+    print("Working directory : {}".format(os.getcwd()))
+    assert cfg.node.loompa == 10  # attribute style access
+    assert cfg["node"]["loompa"] == 10  # dictionary style access
+
+    assert cfg.node.zippity == 10  # Value interpolation
+    assert isinstance(cfg.node.zippity, int)  # Value interpolation type
+    assert cfg.node.do == "oompa 10"  # string interpolation
+
+    cfg.node.waldo  # raises an exception
+
+
+if __name__ == "__main__":
+    my_app()
